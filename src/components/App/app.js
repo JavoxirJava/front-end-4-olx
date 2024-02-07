0a5f15fde97e601373198e3126305b89cd80f5ec
NurmuhammadBozorov <nbazarovv0119@gmail.com>
2024-02-07 15:43:28 +0500

    Navbar

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import Profile from '../Profile/profile'
 import Product from '../Product/product'
 import Messenge from '../Messenge/messenge'
+import HomePage from '../Home-Page/home-page'
+import Navbar from '../Navbar/navbar'
 const App = () => {
   return (
     <div>
@@ -9,6 +11,8 @@ const App = () => {
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/product' element={<Product/>}/>
         <Route path='/messenge' element={<Messenge/>}/>
+        <Route path='/home-page' element={<HomePage/>}/>
+        <Route path='/navbar' element={<Navbar/>}/>
       </Routes>
     </div>
   )
---
+++ src/components/Home-Page/home-page.js
@@ -0,0 +1,11 @@
+import './home-page.css'
+
+const HomePage = () => {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default HomePage
